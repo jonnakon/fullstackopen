f9d0498653fa8eb1a157d471fe4bb703a90e2120
Jonna Konttila <jonna.konttila@gmail.com>
2024-01-15 11:52:09 +0200

    frontend dist added

--- part3/index.js
+++ part3/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('content', (request) =>
   request.method === 'POST' && request.body.name
